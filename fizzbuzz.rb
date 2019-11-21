ba60c147afd6aac8c73ea80273ea585c6d1a9551
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-11-21 21:54:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-require rspec
+
 
 def fizzbuzz(i)
   if i % 3 and i % 5 == 0
